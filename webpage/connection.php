0857acc2a6ecc5966b388819663c8c31b019512c
My Cole <78054380+MaiColee@users.noreply.github.com>
2022-09-05 03:12:56 +0100

    Update and rename connection.php to webpage/connection.php

--- webpage/connection.php
+++ webpage/connection.php
@@ -5,7 +5,7 @@ $DBHost = '127.0.0.1'; // e.g 'localhost' or '127.0.0.1'
 $DBportNr = '3306'; //MySQL Server port nr.
 $DBUser   = 'root';  //the MySQL user name
 $DBPassword   = '';  //the MySQL user password
-$DBName   = 'iot'; //the MySQL database name
+$DBName   = '#DBName'; //the MySQL database name
 //
 
 ?>
